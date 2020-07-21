db95f0497c406b0011b09ffa7adedfa3e094e860
Yuriy Martini <yuriy.martini@live.it>
2020-07-21 09:57:24 +0200

    wip

--- src/Traits/HasSubscriptions.php
+++ src/Traits/HasSubscriptions.php
@@ -2,9 +2,13 @@
 
 namespace YuriyMartini\Subscriptions\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Facades\DB;
+use YuriyMartini\Subscriptions\Enums\SubscriptionStatus;
 
 /**
  * @mixin Model
@@ -25,5 +29,28 @@ trait HasSubscriptions
     public function subscriptions()
     {
         return $this->hasMany(static::getSubscriptionContractBinding());
+    }
+
+    public function hasActiveSubscriptions()
+    {
+        return $this->subscriptions()->where('status', SubscriptionStatus::ACTIVE)->exists();
+    }
+
+    /**
+     * @param  Builder|QueryBuilder  $query
+     */
+    public static function scopeActiveSubscriptions($query)
+    {
+        $query
+            ->whereExists(function (QueryBuilder $query) {
+                $hasSubscriptions = static::resolveHasSubscriptionsContract();
+                $subscription = static::resolveSubscriptionContract();
+
+                $query
+                    ->select(DB::raw(1))
+                    ->from($subscription->getTable())
+                    ->whereRaw("{$hasSubscriptions->getTable()}.{$hasSubscriptions->getKeyName()} = {$subscription->getTable()}.{$hasSubscriptions->getForeignKey()}")
+                    ->where("{$subscription->getTable()}.status", SubscriptionStatus::ACTIVE);
+            });
     }
 }
